5cab7829f3492a358ec3aaccf8198d5011be796d
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-06-22 23:52:58 +0700

    Initial commit

---
+++ search/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+import wikipedia
+
+def index(request):
+    wiki = wikipedia.summary("Wikipedia")
+    search = wikipedia.search("Judas Iscariot")
+    print(search)
+    return JsonResponse({
+        # 'wiki': wiki,
+        'search': search
+    })
